c9ae7b3b3d75e3215cf84e8e839957a30936a114
Rohit Kumar <rajabit111@gmail.com>
2025-03-16 14:54:08 +0000

    changes to schema

--- models/marts/mart_airbnb.sql
+++ models/marts/mart_airbnb.sql
@@ -1,3 +1,10 @@
+{{ config(
+    materialized='incremental',
+    unique_key='listing_id',
+    on_schema_change='append_new_columns'
+) }}
+
+WITH final_data AS (
 SELECT 
     cal.listing_id,
     cal.date,
@@ -9,4 +16,9 @@ SELECT
 FROM {{ ref('int_calendar_metrics') }} cal
 LEFT JOIN {{ ref('stg_listings') }} lst ON cal.listing_id = lst.listing_id
 LEFT JOIN {{ ref('stg_reviews') }} rev ON cal.listing_id = rev.listing_id
-LEFT JOIN {{ ref('int_amenities') }} amen ON cal.listing_id = amen.listing_id;
+LEFT JOIN {{ ref('int_amenities') }} amen ON cal.listing_id = amen.listing_id
+)
+SELECT * FROM final_data
+{% if is_incremental() %}
+    WHERE date > (SELECT MAX(date) FROM {{ this }})  -- ✅ Process only new data
+{% endif %}
